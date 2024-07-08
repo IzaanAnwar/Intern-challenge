e4f3141dd2401156578b6bc5744b676e4dcce150
IzaanAnwar <mdizaan67@gmail.com>
2024-07-08 13:38:31 +0530

    feat: prisma schema

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,16 +1,2 @@
-import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-import * as schema from '../models/index';
-dotenv.config();
-
-const url = process.env.DATABASE_URL;
-
-if (!url) {
-  throw new Error('Enviroment variables missing');
-}
-const connection = mysql.createPool({
-  uri: url,
-});
-
-export const db = drizzle(connection, { schema: schema, mode: 'default' });
+import { PrismaClient } from '@prisma/client';
+export const db = new PrismaClient();

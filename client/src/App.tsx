bcc2182828f579aad6fc8d1073d3ed79b1c52e2b
IzaanAnwar <mdizaan67@gmail.com>
2024-07-09 00:50:06 +0530

    refactor: profile path added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import { Dashboard } from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );

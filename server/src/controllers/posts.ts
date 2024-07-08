8056c766f3c8ae7458eefb4c0f2d944ecf09c770
IzaanAnwar <mdizaan67@gmail.com>
2024-07-09 01:19:36 +0530

    feat: add score when user create posts

--- server/src/controllers/posts.ts
+++ server/src/controllers/posts.ts
@@ -5,7 +5,7 @@ import { db } from '../config/db';
 import { createPostSchema } from '../utils/zod-schema';
 
 dotenv.config();
-
+const NEW_POST_SCORE = 10;
 export async function createPost(req: Request, res: Response, next: NextFunction) {
   const body = await req.body;
   const user = await req.user;
@@ -15,19 +15,32 @@ export async function createPost(req: Request, res: Response, next: NextFunction
 
   try {
     const bodyParsed = createPostSchema.parse(body);
-
-    const postCreated = await db.post.create({
-      data: {
-        title: bodyParsed.title,
-        body: bodyParsed.body,
-        authorId: user.userId,
-      },
-    });
-    if (!postCreated) {
+    const transcationRes = await db.$transaction([
+      db.post.create({
+        data: {
+          title: bodyParsed.title,
+          body: bodyParsed.body,
+          authorId: user.userId,
+        },
+      }),
+      db.scores.upsert({
+        where: { userId: user.userId },
+        create: {
+          userId: user.userId,
+          score: NEW_POST_SCORE,
+        },
+        update: {
+          score: {
+            increment: NEW_POST_SCORE,
+          },
+        },
+      }),
+    ]);
+    if (!transcationRes) {
       throw new Error('Could not create post');
     }
 
-    return res.status(201).json({ postId: postCreated.id });
+    return res.status(201).json({ postId: transcationRes[0].id });
   } catch (error) {
     next(error); // Pass errors to your error-handling middleware
   }

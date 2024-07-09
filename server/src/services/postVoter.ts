e70b1bbff50b0b5ed4a77f39d9f7832c1f99ea53
IzaanAnwar <mdizaan67@gmail.com>
2024-07-10 02:03:22 +0530

    chore: add downvotee

--- server/src/services/postVoter.ts
+++ server/src/services/postVoter.ts
@@ -10,3 +10,12 @@ export async function addUpvote(postId: string, userId: string) {
   });
   return upvote;
 }
+
+export async function addDownvotee(postId: string, userId: string) {
+  const upvote = await db.upvote.upsert({
+    where: { postId_userId: { postId, userId } },
+    update: { value: DOWNVOTE },
+    create: { postId, userId, value: -1 },
+  });
+  return upvote;
+}

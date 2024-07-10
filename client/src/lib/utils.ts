6e5040e8530839d5df79c474411f1a5061f18370
IzaanAnwar <mdizaan67@gmail.com>
2024-07-10 16:21:57 +0530

    chore: get curr user id from cookies storage

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import Cookies from 'js-cookie';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getCurrentUserId() {
+  return Cookies.get('user_id');
 }

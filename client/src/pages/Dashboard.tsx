b516f46f52a9c60c86de166d6e010242748c7212
IzaanAnwar <mdizaan67@gmail.com>
2024-07-11 01:56:26 +0530

    style: show create posts with a btn click + fade animation

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -22,6 +22,8 @@ export function Dashboard() {
   const [body, setBody] = useState<string>('');
   const [isLoading, setIsLoading] = useState(true);
   const [isMutationPending, setIsMutationPending] = useState(false);
+  const [showCreatePostView, setShowCreatePostView] = useState(false);
+
   const createPost = async () => {
     if (!title || !body) {
       toast.error('Title or Body Missing');
@@ -35,6 +37,7 @@ export function Dashboard() {
       }
       const allPosts = res.data?.posts as Post[] | undefined;
       setPosts(allPosts);
+      setShowCreatePostView(false);
     } catch (error: any) {
       let errMsg = '';
       if (error instanceof AxiosError) {
@@ -106,38 +109,45 @@ export function Dashboard() {
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <div className="mx-auto grid w-full md:w-[70%] lg:w-[60%] flex-1 auto-rows-max gap-4">
-            <Card x-chunk="dashboard-07-chunk-0">
-              <CardHeader>
-                <CardTitle>Create Post</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="grid gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="title">Title</Label>
-                    <Input id="title" name="title" onChange={(e) => setTitle(e.target.value)} />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="body">Title</Label>
+            {!showCreatePostView && (
+              <Button className="w-fit animate-fade" onClick={() => setShowCreatePostView(true)}>
+                New Post
+              </Button>
+            )}
+            {showCreatePostView && (
+              <Card className="animate-fade">
+                <CardHeader>
+                  <CardTitle>Create Post</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid gap-6">
+                    <div className="grid gap-3">
+                      <Label htmlFor="title">Title</Label>
+                      <Input id="title" name="title" onChange={(e) => setTitle(e.target.value)} />
+                    </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor="body">Title</Label>
 
-                    <Textarea id="body" name="body" className="min-h-24" onChange={(e) => setBody(e.target.value)} />
+                      <Textarea id="body" name="body" className="min-h-24" onChange={(e) => setBody(e.target.value)} />
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button
-                  variant="default"
-                  className="flex justify-start items-center gap-2 ml-auto"
-                  onClick={createPost}
-                  loading={isMutationPending}
-                  disabled={isMutationPending}
-                >
-                  <DessertIcon />
-                  <p>Post</p>
-                </Button>
-              </CardFooter>
-            </Card>
+                </CardContent>
+                <CardFooter>
+                  <Button
+                    variant="default"
+                    className="flex justify-start items-center gap-2 ml-auto"
+                    onClick={createPost}
+                    loading={isMutationPending}
+                    disabled={isMutationPending}
+                  >
+                    <DessertIcon />
+                    <p>Post</p>
+                  </Button>
+                </CardFooter>
+              </Card>
+            )}
             <section className="space-y-6">
-              <h1>Latest Posts</h1>
+              <h1 className="text-lg font-bold">Latest Posts</h1>
               {posts?.map((post, i) => {
                 const currUserId = getCurrentUserId();
                 const isUpvoted = post.upvote.find((item) => item.userId === currUserId);
